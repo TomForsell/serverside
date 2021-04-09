f80fd3a4443de5e0c1f08b4c4caa6121994e5a29
Tom Forsell <77614564+TomForsell@users.noreply.github.com>
2021-04-09 14:38:38 +0200

    Delete is now not working

--- src/main/java/no/dnb/serverside/models/ConfigData.java
+++ src/main/java/no/dnb/serverside/models/ConfigData.java
@@ -17,7 +17,7 @@ public class ConfigData {
 //test
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id = -1;
+    private long configID = -1;
 
     @ManyToOne
     @JoinColumn(name="ENVIRONMENT_ID")
@@ -31,15 +31,15 @@ public class ConfigData {
     public ConfigData() { }
 
     public ConfigData(Environment environment, String keyName, String configValue) {
-        this.id = id;
+        this.configID = configID;
         this.environment = environment;
         this.keyName = keyName;
         this.configValue = configValue;
         this.ts = new Timestamp(date.getTime());
     }
 
-    public long getId() {
-        return id;
+    public long getConfigID() {
+        return configID;
     }
 
     public Environment getEnvironment() {
@@ -58,8 +58,8 @@ public class ConfigData {
         return ts;
     }
 
-    public void setId(long configID) {
-        this.id = configID;
+    public void setConfigID(long configID) {
+        this.configID = configID;
     }
 
     public void setEnvironment(Environment environment) {
